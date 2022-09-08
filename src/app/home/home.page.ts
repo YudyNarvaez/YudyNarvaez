458a6919f0f874ebb3309c3cdf3360aa268292f7
juan-Ortiz10 <68971989+juan-Ortiz10@users.noreply.github.com>
2022-09-08 09:56:47 -0500

    Imagenes - API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router, private http: HttpClient) {}
 
+  analizarEmocion(text: string){
+    const url = 'https://xoespgkaoe.execute-api.us-west-1.amazonaws.com/prod/sentimiento/';
+    const headers = { 'Content-Type': 'application/json' };
+    const body = { 'sentencia': text };
+    this.http.post<any>(url, body, { headers }).subscribe({
+        next: data => {
+          setTimeout(() => {
+            this.router.navigate(['/results'], {state : { data}});
+          }, 2000);
+        },
+        error: error => {
+            console.error('There was an error!', error);
+        }
+    });
+  }
 }
--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsPage implements OnInit {
 
-  constructor() { }
+  emotion;
+  probs;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.emotion = this.router.getCurrentNavigation().extras.state.data.emocion;
+    this.probs = this.router.getCurrentNavigation().extras.state.data.probs;
+    switch (this.emotion) {
+      case 'others':
+        this.emotion = 'OTRAS';
+        break;
+      case 'joy':
+        this.emotion = 'ALEGRÍA';
+        break;
+      case 'sadness':
+        this.emotion = 'TRISTEZA';
+        break;
+      case 'surprise':
+        this.emotion = 'SORPRESA';
+        break;
+      case 'disgust':
+        this.emotion = 'DESAGRADO';
+        break;
+      case 'anger':
+        this.emotion = 'ENOJO';
+        break;
+    }
   }
-
 }
